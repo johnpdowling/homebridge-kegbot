4ca1e1c43b5541e27d0c00e20bc62c839a83e77f
John Dowling <john.patrick.dowling@gmail.com>
2018-03-27 03:13:09 -0700

    index.js first draft
    With heavy borrowing from homebridge-mcuiot, first run at a detection scheme, never ran.

--- index.js
+++ index.js
@@ -14,50 +14,28 @@ module.exports = function(homebridge) {
   
   // For platform plugin to be considered as dynamic platform plugin,
   // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
-  homebridge.registerPlatform("homebridge-samplePlatform", "SamplePlatform", SamplePlatform, true);
+  homebridge.registerPlatform("homebridge-kegbot", "KegbotPlatform", KegbotPlatform, true);
 }
 
 // Platform constructor
 // config may be null
 // api may be null if launched from old homebridge version
-function SamplePlatform(log, config, api) {
-  log("SamplePlatform Init");
+function KegbotPlatform(log, config, api) {
+  log("KegbotPlatform Init");
   var platform = this;
   this.log = log;
   this.config = config;
   this.accessories = [];
 
-  this.requestServer = http.createServer(function(request, response) {
-    if (request.url === "/add") {
-      this.addAccessory(new Date().toISOString());
-      response.writeHead(204);
-      response.end();
-    }
-
-    if (request.url == "/reachability") {
-      this.updateAccessoriesReachability();
-      response.writeHead(204);
-      response.end();
-    }
-
-    if (request.url == "/remove") {
-      this.removeAccessory();
-      response.writeHead(204);
-      response.end();
-    }
-  }.bind(this));
-
-  this.requestServer.listen(18081, function() {
-    platform.log("Server Listening...");
-  });
-
+  this.refresh = config['refresh'] || 60; // Update every minute
+  this.url = config['url'] || "localhost";
+  
+  if (typeof(config.aliases) !== "undefined" && config.aliases !== null) {
+    this.aliases = config.aliases;
+  }
+  
   if (api) {
-      // Save the API object as plugin needs to register new accessory via this object
       this.api = api;
-
-      // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories.
-      // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-      // Or start discover new accessories.
       this.api.on('didFinishLaunching', function() {
         platform.log("DidFinishLaunching");
       }.bind(this));
@@ -67,7 +45,7 @@ function SamplePlatform(log, config, api) {
 // Function invoked when homebridge tries to restore cached accessory.
 // Developer can configure accessory at here (like setup event handler).
 // Update current value.
-SamplePlatform.prototype.configureAccessory = function(accessory) {
+KegbotPlatform.prototype.configureAccessory = function(accessory) {
   this.log(accessory.displayName, "Configure Accessory");
   var platform = this;
 
@@ -75,144 +53,162 @@ SamplePlatform.prototype.configureAccessory = function(accessory) {
   // otherwise set to false and update the reachability later by invoking 
   // accessory.updateReachability()
   accessory.reachable = true;
-
+  
   accessory.on('identify', function(paired, callback) {
     platform.log(accessory.displayName, "Identify!!!");
     callback();
   });
+  
+  //accessory.on('identify', function(paired, callback) {
+  //  platform.log(accessory.displayName, "Identify!!!");
+  //  callback();
+  //});
 
-  if (accessory.getService(Service.Lightbulb)) {
-    accessory.getService(Service.Lightbulb)
-    .getCharacteristic(Characteristic.On)
-    .on('set', function(value, callback) {
-      platform.log(accessory.displayName, "Light -> " + value);
-      callback();
-    });
+  if (accessory.getService(Service.TemperatureSensor)) {
+
+    accessory.log = this.log;
+////    accessory.loggingService = new FakeGatoHistoryService("weather", accessory,4032,this.refresh * 10/60);
+//    accessory.loggingService = new FakeGatoHistoryService("weather", accessory,{
+//      storage: this.storage,
+//      minutes: this.refresh * 10/60
+//    });
+
+    
   }
 
-  this.accessories.push(accessory);
+  var name = accessory.context.name;
+  self.accessories[name] = accessory;
 }
 
 // Handler will be invoked when user try to config your plugin.
 // Callback can be cached and invoke when necessary.
-SamplePlatform.prototype.configurationRequestHandler = function(context, request, callback) {
-  this.log("Context: ", JSON.stringify(context));
-  this.log("Request: ", JSON.stringify(request));
-
-  // Check the request response
-  if (request && request.response && request.response.inputs && request.response.inputs.name) {
-    this.addAccessory(request.response.inputs.name);
-
-    // Invoke callback with config will let homebridge save the new config into config.json
-    // Callback = function(response, type, replace, config)
-    // set "type" to platform if the plugin is trying to modify platforms section
-    // set "replace" to true will let homebridge replace existing config in config.json
-    // "config" is the data platform trying to save
-    callback(null, "platform", true, {"platform":"SamplePlatform", "otherConfig":"SomeData"});
-    return;
-  }
-
-  // - UI Type: Input
-  // Can be used to request input from user
-  // User response can be retrieved from request.response.inputs next time
-  // when configurationRequestHandler being invoked
-
-  var respDict = {
-    "type": "Interface",
-    "interface": "input",
-    "title": "Add Accessory",
-    "items": [
-      {
-        "id": "name",
-        "title": "Name",
-        "placeholder": "Fancy Light"
-      }//, 
-      // {
-      //   "id": "pw",
-      //   "title": "Password",
-      //   "secure": true
-      // }
-    ]
-  }
-
-  // - UI Type: List
-  // Can be used to ask user to select something from the list
-  // User response can be retrieved from request.response.selections next time
-  // when configurationRequestHandler being invoked
-
-  // var respDict = {
-  //   "type": "Interface",
-  //   "interface": "list",
-  //   "title": "Select Something",
-  //   "allowMultipleSelection": true,
-  //   "items": [
-  //     "A","B","C"
-  //   ]
-  // }
-
-  // - UI Type: Instruction
-  // Can be used to ask user to do something (other than text input)
-  // Hero image is base64 encoded image data. Not really sure the maximum length HomeKit allows.
-
-  // var respDict = {
-  //   "type": "Interface",
-  //   "interface": "instruction",
-  //   "title": "Almost There",
-  //   "detail": "Please press the button on the bridge to finish the setup.",
-  //   "heroImage": "base64 image data",
-  //   "showActivityIndicator": true,
-  // "showNextButton": true,
-  // "buttonText": "Login in browser",
-  // "actionURL": "https://google.com"
-  // }
-
-  // Plugin can set context to allow it track setup process
-  context.ts = "Hello";
-
-  // Invoke callback to update setup UI
-  callback(respDict);
+KegbotPlatform.prototype.configurationRequestHandler = function(context, request, callback) {
+  this.log("configurationRequestHandler");
 }
 
-// Sample function to show how developer can add accessory dynamically from outside event
-SamplePlatform.prototype.addAccessory = function(accessoryName) {
+Kegbot.prototype.didFinishLaunching = function() {
+  var self = this;
+  
+  setInterval(this.devicePolling.bind(this), this.refresh * 1000);
+}
+
+Kegbot.prototype.devicePolling = function() {
+  var self = this;
+  this.log("Reading Thermosensors");
+
+  httpRequest(this.url + "/api/thermo-sensors/", "", "GET", function(err, response, responseBody) {
+    if (err) {
+      this.log('HTTP get failed:', err.message);
+    } else {
+      var response = JSON.parse(responseBody);
+
+      if (response.meta.result != "ok") {
+        self.log("Error status %s %s", response.error.code, response.error.message);
+      } else {
+        
+        response.objects.forEach((thermo, index) => 
+        {
+          if(thermo.sensor_name in this.accessories)
+          {
+            this.accessories[thermo.sensor_name].getService(Service.TemperatureSensor).
+              getCharacteristic(Characteristic.CurrentTemperature).updateValue(thermo.last_log.temperature_c);
+          }
+          else
+          {
+            this.addAccessory(thermo.sensor_name);
+          }
+        });
+          
+      }
+    }
+  }.bind(self));  
+  for (var id in this.accessories) {
+    var device = this.accessories[id];
+    if (device.reachable) {
+      if (device.getService(Service.TemperatureSensor)) {
+        this.getDHTTemperature(device, function(err, temp) {
+          if (err) {
+            temp = err;
+          }
+          this.getService(Service.TemperatureSensor).getCharacteristic(Characteristic.CurrentTemperature).updateValue(temp);
+        }.bind(device));
+      }
+    }
+  }
+}
+
+Kegbot.prototype.addAccessory = function(accessoryName) {
   this.log("Add Accessory");
   var platform = this;
   var uuid;
 
   uuid = UUIDGen.generate(accessoryName);
 
-  var newAccessory = new Accessory(accessoryName, uuid);
-  newAccessory.on('identify', function(paired, callback) {
-    platform.log(accessory.displayName, "Identify!!!");
-    callback();
-  });
-  // Plugin can save context on accessory to help restore accessory in configureAccessory()
-  // newAccessory.context.something = "Something"
-  
-  // Make sure you provided a name for service, otherwise it may not visible in some HomeKit apps
-  newAccessory.addService(Service.Lightbulb, "Test Light")
-  .getCharacteristic(Characteristic.On)
-  .on('set', function(value, callback) {
-    platform.log(accessory.displayName, "Light -> " + value);
-    callback();
-  });
+  if(!this.accessories[accessoryName])
+  {
+    var displayName = this.aliases[accessoryName];
+    if (typeof(displayName) == "undefined") {
+      displayName = accessoryName;
+    }
+    var accessory = new Accessory(accessoryName, uuid, 10);
 
-  this.accessories.push(newAccessory);
-  this.api.registerPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", [newAccessory]);
-}
+    self.log("Adding Thermo Device:", accessoryName, displayName);
+    accessory.reachable = true;
+    accessory.context.model = "Kegbot Thermo";
+    accessory.context.name = accessoryName;
+    accessory.context.displayName = displayName;
 
-SamplePlatform.prototype.updateAccessoriesReachability = function() {
-  this.log("Update Reachability");
-  for (var index in this.accessories) {
-    var accessory = this.accessories[index];
-    accessory.updateReachability(false);
+    accessory.addService(Service.TemperatureSensor, displayName)
+      .getCharacteristic(Characteristic.CurrentTemperature)
+      .setProps({
+        minValue: -100,
+        maxValue: 100
+      });
+    
+    accessory.getService(Service.AccessoryInformation)
+      .setCharacteristic(Characteristic.Manufacturer, "Kegbot")
+      .setCharacteristic(Characteristic.Model, "Kegbot Thermo")
+      .setCharacteristic(Characteristic.SerialNumber, "kb." + accessoryName)
+      .setCharacteristic(Characteristic.FirmwareRevision, require('./package.json').version);
+    
+    accessory.on('identify', function(paired, callback) {
+      platform.log(accessory.displayName, "Identify!!!");
+      callback();
+    });
+    
+    accessory.log = this.log;
+    self.accessories[name] = accessory;
+    self.api.registerPlatformAccessories("homebridge-kegbot", "KegbotPlatform", [accessory]);
+  }
+  else {
+    self.log("Skipping %s", accessoryName);
+    //accessory = this.accessories[name];
+
+    // Fix for devices moving on the network
+    //if (accessory.context.url != url) {
+    //  debug("URL Changed", name);
+    //  accessory.context.url = url;
+    //} else {
+    //  debug("URL Same", name);
+    //}
+    ////        accessory.updateReachability(true);
   }
 }
 
-// Sample function to show how developer can remove accessory dynamically from outside event
-SamplePlatform.prototype.removeAccessory = function() {
-  this.log("Remove Accessory");
-  this.api.unregisterPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", this.accessories);
+function roundInt(string) {
+  return Math.round(parseFloat(string) * 10) / 10;
+}
 
-  this.accessories = [];
+function httpRequest(url, body, method, callback) {
+  request({
+      url: url,
+      body: body,
+      method: method,
+      rejectUnauthorized: false,
+      timeout: 10000
+
+    },
+    function(err, response, body) {
+      callback(err, response, body)
+    })
 }
